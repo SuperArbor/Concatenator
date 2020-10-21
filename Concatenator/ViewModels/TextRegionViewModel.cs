6651d062888d0820e44f2f21cf148bf370dc58af
Bo Yang <snrihc219@hotmail.com>
2020-10-21 19:33:51 +0800

    Project creation

---
+++ Concatenator/ViewModels/TextRegionViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+using Concatenator.Mvvm;
+
+namespace Concatenator.ViewModels
+{
+    class TextRegionViewModel : ObservableObject
+    {
+        private string content;
+        public string Content
+        {
+            get => content; 
+            set => SetProperty(ref content, value); 
+        }
+        public ICommand Concatenate => new RelayCommand(() =>
+        {
+            var pattern = @"([^\.!\?\s]+)(\s*)((\r\n)|\n)(\s*)";
+            if (!string.IsNullOrWhiteSpace(Content))
+            {
+                try
+                {
+                    Content = Regex.Replace(Content, pattern, "$1 ");
+                }
+                catch { }
+            }
+        });
+        public ICommand Copy => new RelayCommand(() =>
+        {
+            if(!string.IsNullOrEmpty(Content))
+                Clipboard.SetText(Content);
+        });
+        public ICommand Clear => new RelayCommand(() =>
+        {
+            Content = string.Empty;
+        });
+    }
+}
---
+++ Concatenator/Views/TextRegion.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Concatenator.ViewModels;
+
+namespace Concatenator.Views
+{
+    /// <summary>
+    /// TextRegion.xaml 的交互逻辑
+    /// </summary>
+    public partial class TextRegion : Window
+    {
+        public TextRegion()
+        {
+            InitializeComponent();
+            DataContext = new TextRegionViewModel();
+        }
+    }
+}
